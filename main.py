fa49376d5057193a43bce9c4c20c6f58ad6b2eda
Boris Gazur <barlukh@gmail.com>
2024-11-27 15:14:34 +0200

    Draw 100 robots with offset on x- and y-axis

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 """ Main execution file for the game. """
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+robot = pygame.image.load("robot.png")
+
+window.fill((0, 0, 0))
+
+width = robot.get_width()
+height = robot.get_height()
+
+def draw_robots():
+    x = 60
+    y = 100
+    for i in range(10):
+        for i in range(10):
+            window.blit(robot, (x + (i*39), y))
+        x += 7
+        y += 20
+
+draw_robots()
+
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
